a156409fb8ea441401a8549a4f017b6edc40fb62
Mark Vollmann <mark.vollmann@elbkind.eu>
2022-03-16 16:44:37 +0100

    feat: 3-2

--- src/day_three.rs
+++ src/day_three.rs
@@ -22,15 +22,8 @@ pub mod first {
     }
 
     #[allow(dead_code)]
-    pub fn run(input: &str) -> Option<usize> {
-        let number_of_bits: usize = match input.lines().next() {
-            None => 0,
-            Some(chars) => chars.len(),
-        };
-
-        if number_of_bits == 0 {
-            return None;
-        }
+    pub fn compute_power_consumption(input: &str) -> usize {
+        let number_of_bits: usize = input.lines().next().expect("Could not read lines").len();
 
         // column entry < 0 => more zeroes than ones
         // column entry > 0 => less zeroes than ones
@@ -49,6 +42,93 @@ pub mod first {
         let gamma = compute_gamma(&bits_accumulate);
         let epsilon = compute_epsilon(&bits_accumulate);
 
-        Some(gamma * epsilon)
+        gamma * epsilon
+    }
+}
+
+pub mod second {
+
+    /**
+     * accepts a list of strings of same length
+     * every char is either '1' or '0'
+     *
+     * This finds the dominant bit for a given column
+     */
+    fn find_dominant_bit(input: &Vec<String>, column: usize, flip: bool) -> i32 {
+        let buffer: i32 =
+            input
+                .iter()
+                .map(|line| line.chars().nth(column))
+                .fold(0, |acc, chat_at_index| match chat_at_index {
+                    Some('0') => acc - 1,
+                    Some('1') => acc + 1,
+                    _ => acc,
+                });
+
+        if flip {
+            if buffer >= 0 {
+                return 0;
+            }
+
+            return 1;
+        }
+
+        if buffer >= 0 {
+            return 1;
+        }
+
+        0
+    }
+
+    fn compute_oxygen(lines: &Vec<String>, index: usize) -> usize {
+        compute_and_filter(lines, index, false)
+    }
+
+    fn compute_co2_scrubber(lines: &Vec<String>, index: usize) -> usize {
+        compute_and_filter(lines, index, true)
+    }
+
+    /**
+     * Count bits in column <index> and filter lines according to flip
+     *
+     * flip = false removes lines with the less occuring bit
+     * flip = true removes lines with the more occuring bit
+     *
+     * recurses until only one line is left
+     */
+    fn compute_and_filter(lines: &Vec<String>, index: usize, flip: bool) -> usize {
+        let dominant_bit = find_dominant_bit(lines, index, flip);
+
+        // list for next recursion step
+        let filtered_lines: Vec<String> = lines
+            .iter()
+            .filter(|line| match line.chars().nth(index) {
+                Some('0') => 0 == dominant_bit,
+                Some('1') => 1 == dominant_bit,
+                _ => false,
+            })
+            .cloned()
+            .collect();
+
+        if filtered_lines.len() > 1 {
+            return compute_and_filter(&filtered_lines, index + 1, flip);
+        }
+        let last_line = filtered_lines.get(0).expect("No line found");
+
+        last_line.chars().fold(0, |acc, c| match c {
+            '1' => acc * 2 + 1,
+            '0' => acc * 2,
+            _ => acc,
+        })
+    }
+
+    #[allow(dead_code)]
+    pub fn compute_life_support_rating(input: &str) -> usize {
+        let lines: Vec<String> = input.lines().map(str::to_string).collect();
+
+        let co2_scrubber = compute_co2_scrubber(&lines, 0);
+        let oxygen = compute_oxygen(&lines, 0);
+
+        oxygen * co2_scrubber
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,15 +6,25 @@ mod tests {
 
     #[test]
     fn day_three_first() {
+        let example =
+            "00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010";
+
         let input = std::fs::read_to_string("./src/day_three.txt").expect("Failed to read file");
 
-        assert_eq!(day_three::first::run(&input), Some(2972336));
+        assert_eq!(day_three::first::compute_power_consumption(example), 198);
+        assert_eq!(day_three::first::compute_power_consumption(&input), 2972336);
     }
 
-    // #[test]
-    // fn day_three_second() {
-    //     let input = std::fs::read_to_string("./src/day_three.txt").expect("Failed to read file");
+    #[test]
+    fn day_three_second() {
+        let example =
+            "00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010";
+        assert_eq!(day_three::second::compute_life_support_rating(example), 230);
 
-    //     assert_eq!(day_three::second::run(&input), 2972336);
-    // }
+        let input = std::fs::read_to_string("./src/day_three.txt").expect("Failed to read file");
+        assert_eq!(
+            day_three::second::compute_life_support_rating(&input),
+            3368358
+        );
+    }
 }
